60eb8791d97a44e5980c61de98e8e8757a44ed3c
dinghao <http://192.168.2.200:8080/sensor.git>
2016-12-13 12:20:14 +0800

    wode

---
+++ app/build/intermediates/classes/debug/com/example/nan/myphonestate/ui/fragment/PhoneFragment$$ViewInjector.java
@@ -0,0 +1,21 @@
+// Generated code from Butter Knife. Do not modify!
+package com.example.nan.myphonestate.ui.fragment;
+
+import android.view.View;
+import butterknife.ButterKnife.Finder;
+import butterknife.ButterKnife.Injector;
+
+public class PhoneFragment$$ViewInjector<T extends com.example.nan.myphonestate.ui.fragment.PhoneFragment> implements Injector<T> {
+  @Override public void inject(final Finder finder, final T target, Object source) {
+    View view;
+    view = finder.findRequiredView(source, 2131492992, "field 'mScrollView'");
+    target.mScrollView = finder.castView(view, 2131492992, "field 'mScrollView'");
+    view = finder.findRequiredView(source, 2131492993, "field 'mRecyclerView'");
+    target.mRecyclerView = finder.castView(view, 2131492993, "field 'mRecyclerView'");
+  }
+
+  @Override public void reset(T target) {
+    target.mScrollView = null;
+    target.mRecyclerView = null;
+  }
+}
